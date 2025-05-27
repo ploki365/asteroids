cfd326a632d9b24bd235b41dbc60a572f902ed70
ploki <82970720+ploki365@users.noreply.github.com>
2025-05-27 19:22:06 -0400

    add pygame boilerplate (fr this time)

--- main.py
+++ main.py
@@ -2,9 +2,26 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill("black")
+        
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
